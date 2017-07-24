1fe7958c889f7e0fcb01565dfe0232bc05b0a87d
kdkorthauer <kdkorthauer@gmail.com>
2017-07-24 11:25:00 -0400

    add sticker

---
+++ inst/sticker/sticker.R
@@ -0,0 +1,15 @@
+imgurl <- "https://clipartion.com/wp-content/uploads/2015/11/bird-island-clipart-free-clip-art-images-830x301.png"
+
+sticker(imgurl,
+         package="dmrseq", 
+         p_size=8, 
+         s_x=1,
+         s_y=1.2, 
+         s_width=0.6, 
+         s_height=1,
+         p_x = 0.95,
+         p_y = 0.5,
+         h_color = "goldenrod",
+         h_fill = "forestgreen",
+         p_color = "goldenrod",
+         filename="./inst/sticker/dmrseq.png")
