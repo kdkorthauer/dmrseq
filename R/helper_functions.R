kdkorthauer/dmrseq

commit 52eb831e8f4a3416574676e298297dee55af0045

Bug fix to properly handle multi-factor conditions in coverage check
This commit fixes issue #6 reported by @LKremer - an error was being
thrown in the check of the number of CpGs with nonzero coverage per
factor group in each region when more than 2 groups were specified.
This was due to improper handling of subsetting the design matrix when
`coeff` was a vector instead of scalar.

R/helper_functions.R
@@ -559,6 +559,8 @@ regionScanner <- function(meth.mat = meth.mat, cov.mat = cov.mat, pos = pos,
     # for factor comparisons 
     replicateStatus <- function(candidates, design, coeff, fact){
       levs <- unique(design[, coeff])
+      if(!is(levs, "matrix"))
+        levs <- as.matrix(levs)
       
       if (fact){
         indexRanges <- IRanges(unlist(lapply(candidates, min)), 
@@ -566,9 +568,11 @@ regionScanner <- function(meth.mat = meth.mat, cov.mat = cov.mat, pos = pos,
         cov.mat.cand <- extractROWS(cov.mat, indexRanges)
         
         prev.mat <- rep(TRUE, nrow(cov.mat.cand))
-        for (l in levs){
+        for (l in seq_len(nrow(levs))){
           cov.matl <- DelayedMatrixStats::rowSums2(
-                        cov.mat.cand[,design[, coeff] == l] > 0) > 1 &
+                          cov.mat.cand[,apply(design[, coeff], 1,
+                            function(x) identical(unname(x),
+                                                  unname(levs[l,])))] > 0) > 1 &
                       prev.mat
           prev.mat <- cov.matl
         }

DESCRIPTION
@@ -2,7 +2,7 @@ Package: dmrseq
 Type: Package
 Title: Detection and inference of differentially methylated regions 
   from Whole Genome Bisulfite Sequencing 
-Version: 1.1.8
+Version: 1.1.9
 Author: Keegan Korthauer <keegan@jimmy.harvard.edu>, 
   Sutirtha Chakraborty <statistuta@gmail.com>, 
   Yuval Benjamini <yuvalbenj@gmail.com>, 

commit 0c8256a2dd3cb5c13abfac0e630264960e4684f0

Fix bug introduced in previous commit
This commit fixes a bug introduced in 52eb831e8f4a3416574676e298297dee55af0045.
Need to enforce matrix data type in identical test for matrix subsetting
(by setting drop = FALSE).

R/helper_functions.R
@@ -570,7 +570,7 @@ regionScanner <- function(meth.mat = meth.mat, cov.mat = cov.mat, pos = pos,
         prev.mat <- rep(TRUE, nrow(cov.mat.cand))
         for (l in seq_len(nrow(levs))){
           cov.matl <- DelayedMatrixStats::rowSums2(
-                          cov.mat.cand[,apply(design[, coeff], 1,
+                          cov.mat.cand[,apply(design[, coeff, drop = FALSE], 1,
                             function(x) identical(unname(x),
                                                   unname(levs[l,])))] > 0) > 1 &
                       prev.mat