kdkorthauer/dmrseq

commit 07d01002041519124936934bae4a9c29c2d5da49

Bug fix for #22 reported by @RoseString
Fixing bug in error handling when candidate region has identical methylation
values in all samples. Previously did not properly construct rows of results
matrix in this condition when there are multiple factor levels in the test
statistic.

DESCRIPTION
@@ -2,7 +2,7 @@ Package: dmrseq
 Type: Package
 Title: Detection and inference of differentially methylated regions 
   from Whole Genome Bisulfite Sequencing 
-Version: 1.3.8
+Version: 1.3.9
 Author: Keegan Korthauer <keegan@jimmy.harvard.edu>, 
   Sutirtha Chakraborty <statistuta@gmail.com>, 
   Yuval Benjamini <yuvalbenj@gmail.com>, 

R/helper_functions.R
@@ -779,8 +779,12 @@ regionScanner <- function(meth.mat = meth.mat, cov.mat = cov.mat, pos = pos,
             
             return(df)
         } else {
-            return(data.frame(beta = rep(NA, length(coeff)), stat = NA, 
-                              constant = TRUE))
+            df1 <- data.frame(stat = NA, constant = TRUE)
+            df2 <- data.frame(matrix(nrow = 1, ncol = length(coeff)))
+            # make sure colnames match nonconstant rows
+            names(df2) <- paste0("beta_", unique(dat$g.fac)[-1])
+            df <- cbind(df1, df2) 
+            return(df)
         }
     }
     

commit 91f6983403d533ae9992ad19c5736f0d775307dc

Fix bug introduced by 07d01002041519124936934bae4a9c29c2d5da49 - harmonize column names for returning region stats

DESCRIPTION
@@ -2,7 +2,7 @@ Package: dmrseq
 Type: Package
 Title: Detection and inference of differentially methylated regions 
   from Whole Genome Bisulfite Sequencing 
-Version: 1.3.9
+Version: 1.3.10
 Author: Keegan Korthauer <keegan@jimmy.harvard.edu>, 
   Sutirtha Chakraborty <statistuta@gmail.com>, 
   Yuval Benjamini <yuvalbenj@gmail.com>, 

R/helper_functions.R
@@ -782,7 +782,11 @@ regionScanner <- function(meth.mat = meth.mat, cov.mat = cov.mat, pos = pos,
             df1 <- data.frame(stat = NA, constant = TRUE)
             df2 <- data.frame(matrix(nrow = 1, ncol = length(coeff)))
             # make sure colnames match nonconstant rows
-            names(df2) <- paste0("beta_", unique(dat$g.fac)[-1])
+            if (length(unique(dat$g.fac)[-1]) == 1){
+              names(df2) <- "beta"
+            }else{
+              names(df2) <- paste0("beta_", unique(dat$g.fac)[-1])
+            }
             df <- cbind(df1, df2) 
             return(df)
         }